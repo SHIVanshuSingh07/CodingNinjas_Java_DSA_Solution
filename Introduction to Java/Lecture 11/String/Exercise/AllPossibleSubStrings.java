32c315441b3cf7ef1d5fb52d638eb60a4c7c32c9
Shivanshu singh <sivanshusingh2004@gmail.com>
2024-02-20 21:43:44 +0530

    all posible

---
+++ Introduction to Java/Lecture 11/String/Exercise/AllPossibleSubStrings.java
@@ -0,0 +1,60 @@
+/*
+
+Print All Substrings
+
+For a given input string(str), write a function to print all the possible substrings.
+Substring
+A substring is a contiguous sequence of characters within a string. 
+Example: "cod" is a substring of "coding". Whereas, "cdng" is not as the characters taken are not contiguous
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+Print the total number of substrings possible, where every substring is printed on a single line and hence the total number of output lines will be equal to the total number of substrings.
+Note:
+The order in which the substrings are printed, does not matter.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+abc
+Sample Output 1:
+a 
+ab 
+abc 
+b 
+bc 
+c 
+ Sample Input 2:
+co
+Sample Output 2:
+c 
+co 
+o
+
+
+
+
+------------------------------------------------------------------------------------------------------
+*/
+
+
+
+public class AllPossibleSubStrings {
+    //Coding Ninjas Solution Code
+    public static void printSubstrings(String str) {
+		//Your code goes here
+        int n = str.length(); 
+        for(int len=1;len<=n;len++){
+            for(int start =0;start<=n-len;start++){
+                int end = start+len;
+                System.out.println(str.substring(start, end));
+            }
+        }    
+	} 
+  public static void main(String[] args) {
+      String str = new String("pqrs");
+      printSubstrings(str);    
+  }  
+}
