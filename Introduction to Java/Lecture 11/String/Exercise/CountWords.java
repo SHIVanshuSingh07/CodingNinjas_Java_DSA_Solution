989cf263dc6ba5714a7ae0cb619003dcc8d09e67
Shivanshu singh <sivanshusingh2004@gmail.com>
2024-02-20 22:45:55 +0530

    update

--- Introduction to Java/Lecture 11/String/Exercise/CountWords.java
+++ Introduction to Java/Lecture 11/String/Exercise/CountWords.java
@@ -27,25 +27,29 @@ Sample Output 2:
 
 ------------------------------------------------------------------------------------------------------------------
 */
-public class CountWords{
-    //coding ninjas solution
-    public static int countWords(String str) {	
+import java.util.Scanner;
+
+public class CountWords {
+
+	public static int countWords(String str) {	
 		//Your code goes here
-        int n = str.length();
-        if(n==0){
-            return 0;
-        }
-        int count = 1;
-        for(int i=0;i<n;i++){
-            if(str.charAt(i)==' '){
-                count++;
-            }
-        }
-    return count;
+		int n = str.length();
+		if(n == 0){
+			return 0;
+		}
+		int count = 1;
+		for(int i = 0; i<n; i++ ){
+			if(str.charAt(i) == ' '){
+				count++;
+			}
+		}
+		return count;
 	}
-    public static void main(String[] args) {
-        String str = "hi sumit";
-        int noOfWords = countWords(str);
-        System.out.println(noOfWords);
-    }
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(countWords(str));
+	}
+
 }
