4c11835977c432d108211f8517ebb89447efbda0
Shivanshu singh <sivanshusingh2004@gmail.com>
2024-02-24 00:47:18 +0530

    remove duplicate

---
+++ Introduction to Java/Lecture 11/String/Assignment/removeConsecutiveDuplicate.java
@@ -0,0 +1,60 @@
+/*
+ Problem statement
+For a given string(str), remove all the consecutive duplicate characters.
+
+Example:
+Input String: "aaaa"
+Expected Output: "a"
+
+Input String: "aabbbcc"
+Expected Output: "abc"
+ Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+The only line of output prints the updated string.
+Note:
+You are not required to print anything. It has already been taken care of.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+aabccbaa
+Sample Output 1:
+abcba
+Sample Input 2:
+xxyyzxx
+Sample Output 2:
+xyzx
+			
+
+
+
+--------------------------------------------------------------------------------------------------------
+
+ */
+
+
+
+public class removeConsecutiveDuplicate {
+
+	public static String removeConsecutiveDuplicates(String str) {
+		//Your code goes here
+		String ans = "";
+		for(int i = 0; i<=str.length()-1; i++){
+			if(i<=str.length()-2 && str.charAt(i) == str.charAt(i+1)){
+				continue;
+			}else{
+				ans = ans + str.charAt(i);
+			}
+		}
+		return ans;
+	}
+
+}
